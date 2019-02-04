d793fe82adeb30b8349e652ab8cb48f89af20316
Holly Gunning <holly.jade.gunning@gmail.com>
2019-02-04 21:52:11 +0000

    Part 3 - Advanced Concepts Completed!

--- djangoDB/boards/tests.py
+++ djangoDB/boards/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .views import home, board_topics, new_topic #remove home and board?
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 # Create your tests here.
 
@@ -98,6 +99,12 @@ class NewTopicTests(TestCase):
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
+    def test_contains_form(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
     def test_new_topic_invalid_post_data(self):
         '''
         Invalid post data should not redirect
@@ -105,7 +112,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
@@ -121,3 +130,5 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+   
