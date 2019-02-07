374c7b0588055190f800cd01ec68fcc140f1c90d
Holly Gunning <holly.jade.gunning@gmail.com>
2019-02-07 16:09:27 +0000

    onto listview next

--- djangoDB/boards/views.py
+++ djangoDB/boards/views.py
@@ -3,6 +3,9 @@ from django.db.models import Count
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
+from django.views.generic import UpdateView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 def home(request):
     boards = Board.objects.all()
@@ -53,3 +56,22 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)  
